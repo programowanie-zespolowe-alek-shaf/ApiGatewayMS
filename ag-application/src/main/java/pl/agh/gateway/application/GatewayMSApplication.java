2f66858b5bf1bc687e852a808f6a21b6112607bb
rxrt8 <michal25kos@gmail.com>
2020-04-29 22:00:48 +0200

    add session

--- ag-application/src/main/java/pl/agh/gateway/application/GatewayMSApplication.java
+++ ag-application/src/main/java/pl/agh/gateway/application/GatewayMSApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@ComponentScan(
+        basePackages = {
+                "pl.agh.gateway"
+        }
+)
 public class GatewayMSApplication {
 
     public static void main(String[] args) {
---
+++ ag-application/src/main/java/pl/agh/gateway/application/controller/LoginController.java
@@ -0,0 +1,21 @@
+package pl.agh.gateway.application.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ResponseEntity<String> login(Principal principal, HttpSession session) {
+        if (!session.isNew()) {
+            session.invalidate();
+        }
+        return ResponseEntity.ok("Logged: " + principal.getName());
+    }
+}
