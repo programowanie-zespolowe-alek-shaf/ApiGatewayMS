f3d664901d49b21792e6edb8bf6ec82a5310826a
Karol Stasiak <karsta26@gmail.com>
2020-05-03 23:17:41 +0200

    remove eureka from prod profile

--- ag-application/src/main/java/pl/agh/gateway/application/GatewayMSApplication.java
+++ ag-application/src/main/java/pl/agh/gateway/application/GatewayMSApplication.java
@@ -2,12 +2,10 @@ package pl.agh.gateway.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
 @ComponentScan(
         basePackages = {
